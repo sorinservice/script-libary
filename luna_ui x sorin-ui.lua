e75ac8e6bad0c01de34dc7c0e6fb2b3636ce1a47
sorinservice <nemesisofnocturn@gmail.com>
2025-09-22 08:59:21 +0200

    Update and rename test-file.lua to luna_ui x sorin-ui.lua

---
+++ luna_ui x sorin-ui.lua
@@ -0,0 +1,5 @@
+Hm. Ich hab mal ganz viele der rbxssid sachen entfernt. waren auch ca 1000 oder so... zudem noch n paar kürzungen. Somit wären wir von 6800 Zeilen erstmal bei 5284 Zeilen. Kommst du hier zum ende?
+
+Das ende ist das mit dem studio und eben dann return Luna
+
+100 kb leicher die datei xD
